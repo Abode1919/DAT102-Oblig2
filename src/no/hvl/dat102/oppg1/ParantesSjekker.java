3b946ffac81d791ad47e1df0d61f32226840b46e
Abode1919 <ghosenabdo7@gmail.com>
2025-02-18 11:46:23 +0100

    bruh

--- src/no/hvl/dat102/oppg1/ParantesSjekker.java
+++ src/no/hvl/dat102/oppg1/ParantesSjekker.java
@@ -1,4 +1,37 @@
 package no.hvl.dat102.oppg1;
 
-public class ParantesSjekker {
+import java.util.Stack;
+
+public class ParentesSjekker {
+
+    public boolean sjekkParenteser(String s) {
+        Stack<Character> stabel = new Stack<>();
+        char[] tegn = s.toCharArray();
+
+        for (char c : tegn) {
+            if (erStartParentes(c)) {
+                stabel.push(c);
+            } else if (erSluttParentes(c)) {
+                if (stabel.isEmpty() || !erParentesPar(stabel.pop(), c)) {
+                    return false;
+                }
+            }
+        }
+
+        return stabel.isEmpty();
+    }
+
+    private boolean erStartParentes(char c) {
+        return c == '{' || c == '[' || c == '(';
+    }
+
+    private boolean erSluttParentes(char c) {
+        return c == '}' || c == ']' || c == ')';
+    }
+
+    private boolean erParentesPar(char start, char slutt) {
+        return (start == '{' && slutt == '}') ||
+                (start == '[' && slutt == ']') ||
+                (start == '(' && slutt == ')');
+    }
 }
--- src/no/hvl/dat102/oppg1/ParantesSjekkerTest.java
+++ src/no/hvl/dat102/oppg1/ParantesSjekkerTest.java
@@ -1,4 +1,44 @@
 package no.hvl.dat102.oppg1;
 
-public class ParantesSjekkerTest {
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ParentesSjekkerTest {
+
+    @Test
+    public void testKorrekteParenteser() {
+        ParentesSjekker sjekker = new ParentesSjekker();
+        assertTrue(sjekker.sjekkParenteser("{ [ ( ) ] }"));
+    }
+
+    @Test
+    public void testManglerSluttparentes() {
+        ParentesSjekker sjekker = new ParentesSjekker();
+        assertFalse(sjekker.sjekkParenteser("{ [ ( ) }"));
+    }
+
+    @Test
+    public void testManglerStartparentes() {
+        ParentesSjekker sjekker = new ParentesSjekker();
+        assertFalse(sjekker.sjekkParenteser("[ ( ) ] }"));
+    }
+
+    @Test
+    public void testSluttparentesForTidlig() {
+        ParentesSjekker sjekker = new ParentesSjekker();
+        assertFalse(sjekker.sjekkParenteser("{ [ ( ] ) }"));
+    }
+
+    @Test
+    public void testJavaProgram() {
+        ParentesSjekker sjekker = new ParentesSjekker();
+        String javaprogram = """
+            class HelloWorld {
+                public static void main(String[] args) {
+                    System.out.println("Hello World!");
+                }
+            }
+            """;
+        assertTrue(sjekker.sjekkParenteser(javaprogram));
+    }
 }
