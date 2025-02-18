53f3f3ce8f71327f90f55be77e7ec7b97fde3e8a
h187455 <187455@stud.hvl.no>
2025-02-18 11:59:00 +0100

    Update ParantesSjekkerTest.java

--- src/no/hvl/dat102/oppg1/ParantesSjekkerTest.java
+++ src/no/hvl/dat102/oppg1/ParantesSjekkerTest.java
@@ -3,35 +3,35 @@ package no.hvl.dat102.oppg1;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-public class ParentesSjekkerTest {
+public class ParantesSjekkerTest {
 
     @Test
     public void testKorrekteParenteser() {
-        ParentesSjekker sjekker = new ParentesSjekker();
+        ParantesSjekker sjekker = new ParantesSjekker();
         assertTrue(sjekker.sjekkParenteser("{ [ ( ) ] }"));
     }
 
     @Test
     public void testManglerSluttparentes() {
-        ParentesSjekker sjekker = new ParentesSjekker();
+        ParantesSjekker sjekker = new ParantesSjekker();
         assertFalse(sjekker.sjekkParenteser("{ [ ( ) }"));
     }
 
     @Test
     public void testManglerStartparentes() {
-        ParentesSjekker sjekker = new ParentesSjekker();
+        ParantesSjekker sjekker = new ParantesSjekker();
         assertFalse(sjekker.sjekkParenteser("[ ( ) ] }"));
     }
 
     @Test
     public void testSluttparentesForTidlig() {
-        ParentesSjekker sjekker = new ParentesSjekker();
+        ParantesSjekker sjekker = new ParantesSjekker();
         assertFalse(sjekker.sjekkParenteser("{ [ ( ] ) }"));
     }
 
     @Test
     public void testJavaProgram() {
-        ParentesSjekker sjekker = new ParentesSjekker();
+        ParantesSjekker sjekker = new ParantesSjekker();
         String javaprogram = """
             class HelloWorld {
                 public static void main(String[] args) {
